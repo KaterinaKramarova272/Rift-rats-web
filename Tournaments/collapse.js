e23c966900615b15ec7a22ae161c9aee80f2bef7
Katerina Kramarova <katerina.krama.024@pslib.cz>
2025-08-09 23:48:36 +0200

    Tournaments added and designed

---
+++ Tournaments/collapse.js
@@ -0,0 +1,27 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.display === "block") {
+//       content.style.display = "none";
+//     } else {
+//       content.style.display = "block";
+//     }
+//   });
+// }
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    console.log (content.style.height)
+    if (content.style.height > "0px") {
+      content.style.height = "0px";
+    } else {
+      content.style.height = "400px";
+    }
+  });
+}
