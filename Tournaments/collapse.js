25ddd5756feb320cc044f0e0ca9f8a678c254f52
Katerina Kramarova <katerina.krama.024@pslib.cz>
2025-09-09 18:58:31 +0200

    tournaments + team

--- Tournaments/collapse.js
+++ Tournaments/collapse.js
@@ -1,23 +1,12 @@
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
-// for (i = 0; i < coll.length; i++) {
-//   coll[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var content = this.nextElementSibling;
-//     if (content.style.display === "block") {
-//       content.style.display = "none";
-//     } else {
-//       content.style.display = "block";
-//     }
-//   });
-// }
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+  coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    console.log (content.style.height)
+    console.log(content.style.height);
     if (content.style.height > "0px") {
       content.style.height = "0px";
     } else {
